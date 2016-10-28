4614649294501d1b5dc3b63606aae129cc855ef0
Steve Wang <steve_wang3@yahoo.com>
2016-10-28 12:36:03 -0700

    Update Tabs

--- watermap.js
+++ watermap.js
@@ -25,12 +25,40 @@ for (i = 0; i < locations.length; i++) {
   google.maps.event.addListener(marker, 'click', (function(marker, i) {
     return function() {
       infowindow.setContent(locations[i][0] + '<p style="color:green;"><b>SAFE</b></p>' +
-      '<ul class="tabs">  <li><a href="javascript:void(0)">Tab1</a></li>   <li><a href="javascript:void(0)">Tab2</a></li>  <li><a href="javascript:void(0)">Tab3</a></li>  <li><a href="javascript:void(0)">Tab4</a></li> </ul>' +
-      ' <section class="tab_content" id="tab1"> <p> Tabbed Conternt 1</p>  </section>'   +
-      ' <section class="tab_content" id="tab2"> <p> Tabbed Conternt 2</p>  </section>'   +
-      ' <section class="tab_content" id="tab3"> <p> Tabbed Conternt 3</p>  </section>'   +
-      ' <section class="tab_content" id="tab4"> <p> Tabbed Conternt 4</p>  </section>'  );
+      '<ul class="tab"> <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, \'Tab1\')" id="defaultOpen"> Tab1 </a></li>' +
+      '<li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, \'Tab2\')"> Tab2</a></li>' +  
+      '<li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, \'Tab3\')"> Tab3</a></li>' + 
+      '<li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, \'Tab4\')"> Tab4</a></li> </ul>' +
+
+
+      ' <div id="Tab1" class="tabcontent"><h3>Tab1</h3><p> Tab1 Content </p></div>' +
+      ' <div id="Tab2" class="tabcontent" style = "display:none" ><h3>Tab2</h3><p> Tab2 Content </p></div>' +
+      ' <div id="Tab3" class="tabcontent" style = "display:none"><h3>Tab3</h3><p> Tab3 Content </p></div>' +
+      ' <div id="Tab4" class="tabcontent" style = "display:none"><h3>Tab4</h3><p> Tab4 Content </p></div>' );
       infowindow.open(map, marker);
     }
   })(marker, i));
+}
+
+
+function openTab(evt, cityName) {
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
